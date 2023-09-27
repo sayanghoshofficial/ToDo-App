02adce2d619fb5723a71f4596f57e9bb03d4e974
Sayan Ghosh <99132893+sayanghoshofficial@users.noreply.github.com>
2023-09-27 20:30:07 +0530

    set styling

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import { useValue } from '../context/context';
+import { MdAddCircle } from 'react-icons/md';
 
 const TodoForm = () => {
-    const{todoText,setTodoText,addTodo}= useValue();
-  return (
-    <div className="todo-form">
-                <input
-                    type="text"
-                    placeholder="Add a new todo"
-                    value={todoText}
-                    onChange={(e) => setTodoText(e.target.value)}
-                    onKeyPress={(e) => {
-                        if (e.key === 'Enter') addTodo();
-                    }}
-                />
-                <button onClick={addTodo}>Add</button>
-            </div>
-  )
+    const { todoText, setTodoText, addTodo } = useValue();
+    return (
+        <div className="todo-form">
+            <input
+                type="text"
+                placeholder="Add a new todo..."
+                value={todoText}
+                onChange={(e) => setTodoText(e.target.value)}
+                onKeyPress={(e) => {
+                    if (e.key === 'Enter') addTodo();
+                }}
+            />
+            <button onClick={addTodo}>
+                <MdAddCircle style={{ color: "white" }} size={30} />
+            </button>
+        </div>
+    )
 }
 
 export default TodoForm
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { useValue } from '../context/context'
+import { useValue } from '../context/context';
+import { AiFillDelete } from 'react-icons/ai';
 
 const TodoItem = ({ todo, index }) => {
     const { toggleTodo, removeTodo } = useValue();
     return (
         <div key={index} className={`todo-item ${todo.completed ? 'completed' : ''}`}>
             <span onClick={() => toggleTodo(index)}>{todo.text}</span>
-            <button onClick={() => removeTodo(index)}>Delete</button>
+            <button onClick={() => removeTodo(index)}>
+            <AiFillDelete style={{color:"red"}} size={30} />
+            </button>
         </div>
     )
 }
